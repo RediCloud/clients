863f5e48de6b380ffe10ff5bc9c734740b92cf96
Anton <anton.wiblishauser@gmx.de>
2022-11-23 17:56:01 +0100

    create BukkitConstants

---
+++ api/paper/src/main/kotlin/net/dustrean/api/paper/extensions/BukkitConstants.kt
@@ -0,0 +1,8 @@
+package net.dustrean.api.paper.extensions
+
+import org.bukkit.Bukkit
+import org.bukkit.plugin.java.JavaPlugin
+
+lateinit var plugin: JavaPlugin
+
+val pluginManager = Bukkit.getPluginManager()
