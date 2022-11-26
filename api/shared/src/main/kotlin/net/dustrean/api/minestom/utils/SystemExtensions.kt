68c7398729457e0ecdf7c52cceded377dbdb5dd6
Anton <anton.wiblishauser@gmx.de>
2022-11-26 19:56:49 +0100

    simplify env function

--- api/shared/src/main/kotlin/net/dustrean/api/minestom/utils/SystemExtensions.kt
+++ api/shared/src/main/kotlin/net/dustrean/api/minestom/utils/SystemExtensions.kt
@@ -1,13 +1,3 @@
 package net.dustrean.api.minestom.utils
 
-import net.dustrean.api.minestom.component.component
-
-inline fun <reified T> env(name: String): T = System.getenv(name) as T
-
-fun test() {
-    component("test") {
-        component("sadaw")
-
-        onClickCopy("sadawd")
-    }
-}
+fun env(name: String) = System.getenv(name)
