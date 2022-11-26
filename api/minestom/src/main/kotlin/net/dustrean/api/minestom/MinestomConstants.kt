24a5b8cde4d7ba94967a7271762dcd98c227da44
Anton <anton.wiblishauser@gmx.de>
2022-11-26 19:56:40 +0100

    add more constants

--- api/minestom/src/main/kotlin/net/dustrean/api/minestom/MinestomConstants.kt
+++ api/minestom/src/main/kotlin/net/dustrean/api/minestom/MinestomConstants.kt
@@ -1,8 +1,20 @@
 package net.dustrean.api.minestom
 
 import net.minestom.server.MinecraftServer
+import net.minestom.server.entity.Player
+import net.minestom.server.network.packet.server.SendablePacket
 
 val server = MinecraftServer.init()
 val eventHandler = MinecraftServer.getGlobalEventHandler()
 val commandManager = MinecraftServer.getCommandManager()
 val instanceManager = MinecraftServer.getInstanceManager()
+val packetListenerManager = MinecraftServer.getPacketListenerManager()
+val packetProcessor = MinecraftServer.getPacketProcessor()
+val connectionmanager = MinecraftServer.getConnectionManager()
+
+val onlinePlayers
+    get() = connectionmanager.onlinePlayers
+
+fun Collection<Player>.sendPacket(packet: SendablePacket) = forEach {
+    it.sendPacket(packet)
+}
