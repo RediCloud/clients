504094340cdfda09ea6bc3df42f4e807d7c2b99b
SteinGaming <danialdaryab@gmail.com>
2022-12-08 17:42:41 +0100

    feat: add velocity and optifine support

--- api/minestom/src/main/kotlin/net/dustrean/api/minestom/MinestomMain.kt
+++ api/minestom/src/main/kotlin/net/dustrean/api/minestom/MinestomMain.kt
@@ -1,11 +1,16 @@
 package net.dustrean.api.minestom
 
 import net.dustrean.api.minestom.utils.env
+import net.minestom.server.MinecraftServer
 import net.minestom.server.extras.MojangAuth
+import net.minestom.server.extras.optifine.OptifineSupport
+import net.minestom.server.extras.velocity.VelocityProxy
 
 fun main() {
     server
     MojangAuth.init()
 
+    VelocityProxy.enable(env("VELOCITY_SECRET"))
+    OptifineSupport.enable()
     server.start("0.0.0.0", env("service.bind.port").toInt())
 }
