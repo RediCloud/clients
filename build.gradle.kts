589bc55631e243da8021cb307cccd62ca1aece3b
SteinGaming <danialdaryab@gmail.com>
2022-12-08 18:06:22 +0100

    feat: libloader repositories add gradle properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ allprojects {
                     url = uri("https://repo.dustrean.net/releases")
                     authentication.create<BasicAuthentication>("basic")
                     credentials {
-                        username = System.getenv("DUSTREAN_REPO_USERNAME")
-                        password = System.getenv("DUSTREAN_REPO_PASSWORD")
+                        username = (findProperty("DUSTREAN_REPO_USERNAME") as String?) ?: System.getenv("DUSTREAN_REPO_USERNAME")
+                        password = (findProperty("DUSTREAN_REPO_PASSWORD") as String?) ?: System.getenv("DUSTREAN_REPO_PASSWORD")
                     }
                 }
             }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,8 +11,10 @@ pluginManagement {
             url = uri("https://repo.dustrean.net/releases")
             authentication.create<BasicAuthentication>("basic")
             credentials {
-                username = System.getenv("DUSTREAN_REPO_USERNAME")
-                password = System.getenv("DUSTREAN_REPO_PASSWORD")
+                val DUSTREAN_REPO_USERNAME: String? by settings
+                val DUSTREAN_REPO_PASSWORD: String? by settings
+                username = DUSTREAN_REPO_USERNAME ?: System.getenv("DUSTREAN_REPO_USERNAME")
+                password = DUSTREAN_REPO_PASSWORD ?: System.getenv("DUSTREAN_REPO_PASSWORD")
             }
         }
     }
