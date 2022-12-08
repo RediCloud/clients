5b811eebb68e6b021e20f40817a94559ea33c9cb
SteinGaming <danialdaryab@gmail.com>
2022-12-08 17:59:15 +0100

    fix 2d614a4ab0fba18a8904dc1cf9e1cc39dfbdb85a: add repository for bootstrap

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,19 @@
 plugins {
     id("net.dustrean.libloader") version "1.2.0" apply false
 }
+allprojects {
+    afterEvaluate {
+        if (plugins.hasPlugin("net.dustrean.libloader")) {
+            repositories {
+                maven {
+                    url = uri("https://repo.dustrean.net/releases")
+                    authentication.create<BasicAuthentication>("basic")
+                    credentials {
+                        username = System.getenv("DUSTREAN_REPO_USERNAME")
+                        password = System.getenv("DUSTREAN_REPO_PASSWORD")
+                    }
+                }
+            }
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,11 @@ pluginManagement {
         gradlePluginPortal()
         maven {
             url = uri("https://repo.dustrean.net/releases")
+            authentication.create<BasicAuthentication>("basic")
+            credentials {
+                username = System.getenv("DUSTREAN_REPO_USERNAME")
+                password = System.getenv("DUSTREAN_REPO_PASSWORD")
+            }
         }
     }
 }
