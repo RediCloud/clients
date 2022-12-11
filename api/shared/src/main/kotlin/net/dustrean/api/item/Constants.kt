5029ca136de59c7b70acbbd382dc599ccd8a528a
SteinGaming <danialdaryab@gmail.com>
2022-12-11 17:45:06 +0100

    move: item functions from Constants.kt to ItemFactory.kt

--- api/shared/src/main/kotlin/net/dustrean/api/item/Constants.kt
+++ api/shared/src/main/kotlin/net/dustrean/api/item/Constants.kt
@@ -1,9 +1,5 @@
 package net.dustrean.api.item
 
-import net.dustrean.api.minestom.item.enums.Material
-import net.dustrean.api.minestom.item.factories.impl.DefaultItemFactory
-import net.dustrean.api.minestom.item.factories.impl.DynamicItemFactory
-import net.dustrean.api.minestom.scoreboard.factories.ScoreboardFactory
 import java.util.*
 
 object Constants {
@@ -11,18 +7,5 @@ object Constants {
     fun ItemStack.register(uuid: UUID) {
         items.putIfAbsent(uuid, this) ?: return
         throw IllegalArgumentException("UUID already exists")
-    }
-
-    inline fun item(material: Material, crossinline apply: DefaultItemFactory.() -> Unit): ItemStack {
-        return DefaultItemFactory(material).apply(apply).create()
-            .get(null) // null because we don't need a player for customization.
-    }
-
-    inline fun dynamicItem(noinline material: (UUID?) -> Material, crossinline apply: DynamicItemFactory.() -> Unit): ItemStackLike {
-        return DynamicItemFactory(material).apply(apply)
-    }
-
-    inline fun scoreboard(crossinline init: ScoreboardFactory.() -> Unit): ScoreboardFactory {
-        return ScoreboardFactory().apply(init)
     }
 }
