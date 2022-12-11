2cbf6708fda2882bacdb3ea039341d09124c647a
SteinGaming <danialdaryab@gmail.com>
2022-12-11 12:29:11 +0100

    feat: scoreboard factory

---
+++ api/shared/src/main/kotlin/net/dustrean/api/minestom/scoreboard/factories/ScoreboardFactory.kt
@@ -0,0 +1,34 @@
+package net.dustrean.api.minestom.scoreboard.factories
+
+import net.dustrean.api.minestom.scoreboard.ScoreboardLine
+import net.kyori.adventure.text.Component
+import java.util.*
+
+class ScoreboardFactory {
+    val lines: MutableMap<String, ScoreboardLine> = mutableMapOf()
+    var title: (UUID) -> Component = { Component.empty() }
+
+    inline fun line(crossinline init: LineFactory.() -> Unit) {
+        val line = LineFactory().apply(init).create()
+        lines.putIfAbsent(line.name, line) ?: return
+        throw IllegalArgumentException("Line name already exists")
+    }
+
+    fun empty(score: Int) =
+        line {
+            this.score = score
+            this.name = "empty-$score"
+            this.content = Component.empty()
+        }
+
+    inline fun dynamic(score: Int, name: String, crossinline init: ScoreboardLine.(UUID) -> Unit, startContent: Component = Component.empty()) {
+        line {
+            this.score = score
+            this.name = name
+            this.content = startContent
+            this.proceed = {
+                this.init(it)
+            }
+        }
+    }
+}
