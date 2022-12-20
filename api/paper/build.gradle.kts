916b8b4cc1213c6d46cc39ef64a06fcfd478c7b7
SteinGaming <danialdaryab@gmail.com>
2022-12-20 18:58:14 +0100

    fix: don't use paper-script and implement paper-api ourselves, due to api/paper not being a plugin, only a base for one

--- api/paper/build.gradle.kts
+++ api/paper/build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     `kotlin-script`
-    `paper-script`
     id("net.dustrean.libloader")
 }
 
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
 dependencies {
+    compileOnly("io.papermc.paper:paper-api:${BuildConstants.minecraftVersion}-R0.1-SNAPSHOT")
     shade(project(":api:shared"))
 }
