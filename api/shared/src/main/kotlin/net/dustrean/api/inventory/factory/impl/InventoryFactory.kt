a8a32a6ef9b1fbf9044b8c1681a932e6f5f5b8f3
SteinGaming <danialdaryab@gmail.com>
2022-12-23 13:24:24 +0100

    feat: Inventory API compatible with the Item API

---
+++ api/shared/src/main/kotlin/net/dustrean/api/inventory/factory/impl/InventoryFactory.kt
@@ -0,0 +1,57 @@
+package net.dustrean.api.inventory.factory.impl
+
+import net.dustrean.api.item.ItemStackLike
+import net.dustrean.api.inventory.Inventory
+import net.dustrean.api.item.ItemStack
+import net.kyori.adventure.text.Component
+import java.util.*
+
+class InventoryFactory(override var lines: Byte, override var name: (UUID) -> Component) : Inventory {
+    constructor(lines: Byte, name: Component) : this(lines, { name })
+
+    init {
+        require(lines in 1..6) { "Inventory lines must be between 1 and 6." }
+    }
+
+    override var items: MutableMap<Int, ItemStackLike> = mutableMapOf()
+    var border: ItemStackLike? = null
+        set(value) {
+            field = value
+            if (value == null) return
+            for (i in 0 until lines * 9) {
+                if (i % 9 == 0 || i % 9 == 8 || i < 9 || i > lines * 9 - 9) {
+                    items[i] = value
+                }
+            }
+        }
+
+    operator fun set(i: Int, item: ItemStackLike) {
+        items[i] = item
+    }
+
+    operator fun Int.plusAssign(item: ItemStackLike) {
+        items[this] = item
+    }
+
+    operator fun Int.plus(item: ItemStackLike) {
+        items[this] = item
+    }
+
+    operator fun ItemStack.get(range: IntRange) {
+        items.putAll(range.map {
+            it to this
+        })
+    }
+
+    override fun add(item: ItemStackLike) {
+        var i = 0
+        while (items.containsKey(i)) {
+            i++
+        }
+        items[i] = item
+    }
+
+    override fun clone(): Inventory = InventoryFactory(lines, name).apply {
+        items = this@InventoryFactory.items.toMutableMap()
+    }
+}
