6dad75663d1227dbdfa33fbbb4e0b3ed7fd5b8f5
SteinGaming <danialdaryab@gmail.com>
2022-12-25 16:01:57 +0100

    fix: both repositories

--- buildSrc/src/main/kotlin/core-script.gradle.kts
+++ buildSrc/src/main/kotlin/core-script.gradle.kts
@@ -1,9 +1,17 @@
 repositories {
     maven {
         url = uri(
-            "https://repo.dustrean.net/${
-                if (BuildConstants.coreVersion.endsWith("-SNAPSHOT")) "snapshots" else "releases"
-            }"
+            "https://repo.dustrean.net/snapshots"
+        )
+        authentication.create<BasicAuthentication>("basic")
+        credentials {
+            username = (findProperty("DUSTREAN_REPO_USERNAME") as String?) ?: System.getenv("DUSTREAN_REPO_USERNAME")
+            password = (findProperty("DUSTREAN_REPO_PASSWORD") as String?) ?: System.getenv("DUSTREAN_REPO_PASSWORD")
+        }
+    }
+    maven {
+        url = uri(
+            "https://repo.dustrean.net/releases"
         )
         authentication.create<BasicAuthentication>("basic")
         credentials {
