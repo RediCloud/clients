dade8c5650abf85c67ac23dcace08a86ba053d1d
SteinGaming <danialdaryab@gmail.com>
2022-12-25 16:03:54 +0100

    fix: both repositories in root project for allprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,21 +2,31 @@ plugins {
     id("net.dustrean.libloader") version "1.5.3" apply false
 }
 allprojects {
-    afterEvaluate {
-        if (plugins.hasPlugin("net.dustrean.libloader")) {
-            repositories {
-                maven {
-                    url = uri("https://repo.dustrean.net/releases")
-                    authentication.create<BasicAuthentication>("basic")
-                    credentials {
-                        username = (findProperty("DUSTREAN_REPO_USERNAME") as String?)
-                            ?: System.getenv("DUSTREAN_REPO_USERNAME")
-                        password = (findProperty("DUSTREAN_REPO_PASSWORD") as String?)
-                            ?: System.getenv("DUSTREAN_REPO_PASSWORD")
-                    }
-                }
+    repositories {
+        maven {
+            url = uri("https://repo.dustrean.net/releases")
+            authentication.create<BasicAuthentication>("basic")
+            credentials {
+                username = (findProperty("DUSTREAN_REPO_USERNAME") as String?)
+                    ?: System.getenv("DUSTREAN_REPO_USERNAME")
+                password = (findProperty("DUSTREAN_REPO_PASSWORD") as String?)
+                    ?: System.getenv("DUSTREAN_REPO_PASSWORD")
             }
         }
     }
+    repositories {
+        maven {
+            url = uri("https://repo.dustrean.net/snapshots")
+            authentication.create<BasicAuthentication>("basic")
+            credentials {
+                username = (findProperty("DUSTREAN_REPO_USERNAME") as String?)
+                    ?: System.getenv("DUSTREAN_REPO_USERNAME")
+                password = (findProperty("DUSTREAN_REPO_PASSWORD") as String?)
+                    ?: System.getenv("DUSTREAN_REPO_PASSWORD")
+            }
+        }
+    }
+
+
     (tasks.findByName("jar") as Jar?)?.duplicatesStrategy = DuplicatesStrategy.WARN
 }
