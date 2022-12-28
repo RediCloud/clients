9697ce75a5504454660344786facc8f08e38030b
SteinGaming <danialdaryab@gmail.com>
2022-12-28 18:17:19 +0100

    fix: don't use superclass

--- api/minestom/src/main/kotlin/net/dustrean/api/minestom/boot/loader/MinestomJarLoader.kt
+++ api/minestom/src/main/kotlin/net/dustrean/api/minestom/boot/loader/MinestomJarLoader.kt
@@ -5,7 +5,7 @@ import net.minestom.server.extensions.ExtensionClassLoader
 import java.net.URL
 
 class MinestomJarLoader(loader: Extension) : net.dustrean.libloader.boot.JarLoader {
-    val loader: ExtensionClassLoader = loader::class.java.superclass.classLoader as ExtensionClassLoader
+    val loader: ExtensionClassLoader = loader::class.java.classLoader as ExtensionClassLoader
 
     override fun load(javaFile: URL?) {
         loader.addURL(javaFile!!)
