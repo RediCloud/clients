b78b4d4997cbe384a98fe3c9c0e0c9473b3ae5a4
ChaotenHG <chaoten@neverstopgaming.net>
2022-12-28 19:49:33 +0100

    feat: move PlatformCommand from core to clients

---
+++ api/paper/src/main/kotlin/net/dustrean/api/paper/command/PaperCommand.kt
@@ -0,0 +1,36 @@
+package net.dustrean.api.paper.command
+
+import net.dustrean.api.ICoreAPI
+import net.dustrean.api.command.ICommand
+import net.dustrean.api.command.data.CommandData
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import org.bukkit.command.Command as BukkitCommand
+
+open class PaperCommand(
+    override val commandName: String,
+    override val commandAliases: Array<String>,
+    override val commandDescription: String,
+    override val commandPermission: String
+) : BukkitCommand(commandName, commandDescription, commandPermission, commandAliases.toMutableList()), ICommand {
+
+    override fun execute(sender: CommandSender, commandLabel: String, args: Array<out String>): Boolean {
+        if (sender !is Player) {
+            return false
+        }
+        ICoreAPI.INSTANCE.getCommandManager().handleCommand(PaperCommandActor(sender), this, args.toList())
+        return true
+    }
+
+    override fun tabComplete(sender: CommandSender, alias: String, args: Array<out String>): MutableList<String> {
+        if (sender !is Player) {
+            return mutableListOf()
+        }
+
+        return  ICoreAPI.INSTANCE.getCommandManager().handleTabComplete(PaperCommandActor(sender), this, args.joinToString(" "))
+            .toMutableList()
+    }
+
+    override val commands = arrayListOf<CommandData>()
+
+}
---
+++ api/paper/src/main/kotlin/net/dustrean/api/paper/command/PaperCommandActor.kt
@@ -0,0 +1,13 @@
+package net.dustrean.api.paper.command
+
+import net.dustrean.api.command.ICommandActor
+import org.bukkit.entity.Player
+
+class PaperCommandActor(private val player: Player) : ICommandActor {
+
+    override val uuid = player.uniqueId
+
+    override fun hasPermission(permission: String) = player.hasPermission(permission)
+
+    override fun sendMessage(message: String) = player.sendMessage(message)
+}
