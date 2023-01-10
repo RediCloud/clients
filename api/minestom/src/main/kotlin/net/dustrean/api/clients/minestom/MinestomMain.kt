491b80fcdaf77c926139f4bd04933f080e220882
Suqatri <paul@suqatri.net>
2023-01-10 18:12:08 +0100

    fix minestom loading

--- api/minestom/src/main/kotlin/net/dustrean/api/clients/minestom/MinestomMain.kt
+++ api/minestom/src/main/kotlin/net/dustrean/api/clients/minestom/MinestomMain.kt
@@ -1,17 +1,14 @@
 package net.dustrean.api.clients.minestom
 
-import net.dustrean.api.clients.minestom.item.ItemEvents
 import net.dustrean.api.clients.utils.env
 import net.minestom.server.extras.MojangAuth
 import net.minestom.server.extras.optifine.OptifineSupport
 import net.minestom.server.extras.velocity.VelocityProxy
 
 fun main() {
-    server
-    MojangAuth.init()
-
+    server //to init the val
     VelocityProxy.enable(env("VELOCITY_SECRET"))
     OptifineSupport.enable()
-    MinestomClients()
     server.start(System.getProperty("service.bind.host"), System.getProperty("service.bind.port").toInt())
+    MinestomClients()
 }
