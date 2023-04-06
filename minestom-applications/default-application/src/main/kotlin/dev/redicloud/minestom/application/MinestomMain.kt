61d84ad5276f9615947dc256cb7767476d859e9a
Suqatri <paul@suqatri.net>
2023-04-07 00:18:42 +0200

    fix: non-cloud running minestom

--- minestom-applications/default-application/src/main/kotlin/dev/redicloud/minestom/application/MinestomMain.kt
+++ minestom-applications/default-application/src/main/kotlin/dev/redicloud/minestom/application/MinestomMain.kt
@@ -1,11 +1,22 @@
 package dev.redicloud.minestom.application
 
+import net.minestom.server.MinecraftServer
 import net.minestom.server.extras.optifine.OptifineSupport
 import net.minestom.server.extras.velocity.VelocityProxy
 
 fun main() {
     server //to init the val
-    VelocityProxy.enable(System.getenv("VELOCITY_SECRET"))
+    if (System.getenv("VELOCITY_SECRET") != null) {
+        VelocityProxy.enable(System.getenv("VELOCITY_SECRET"))
+    } else {
+        MinecraftServer.LOGGER.warn("Velocity secret not set, velocity support disabled!")
+    }
     OptifineSupport.enable()
-    server.start(System.getProperty("service.bind.host"), System.getProperty("service.bind.port").toInt())
+    if (System.getProperty("service.bind.host") == null || System.getProperty("service.bind.port") == null) {
+        MinecraftServer.LOGGER.warn("Service bind host or port not set, using default values!")
+    }
+    server.start(
+        System.getProperty("service.bind.host") ?: "127.0.0.1",
+        (System.getProperty("service.bind.port") ?: "5000").toInt()
+    )
 }
