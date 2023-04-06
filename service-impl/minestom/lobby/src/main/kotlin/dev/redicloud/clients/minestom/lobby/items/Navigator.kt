ce028582a3822c8757cbdfc8e7da070e3917ee7b
Suqatri <paul@suqatri.net>
2023-04-07 00:18:15 +0200

    feat: spawn teleport item

--- service-impl/minestom/lobby/src/main/kotlin/dev/redicloud/clients/minestom/lobby/items/Navigator.kt
+++ service-impl/minestom/lobby/src/main/kotlin/dev/redicloud/clients/minestom/lobby/items/Navigator.kt
@@ -1,21 +1,25 @@
 package dev.redicloud.clients.minestom.lobby.items
 
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import dev.redicloud.clients.gui.GuiType
 import dev.redicloud.clients.gui.impl.BaseGui
 import dev.redicloud.clients.item.UnassignedItemStack
 import dev.redicloud.clients.item.enums.InteractType
 import dev.redicloud.clients.item.enums.Material
+import dev.redicloud.minestom.application.getPlayer
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.minestom.server.coordinate.Pos
 
 object Navigator : UnassignedItemStack(material = { Material.COMPASS },
-    properties = { mutableMapOf("index" to 0) },
     languageProvider = {
         {
-            name = Component.text("§aNavigator")
+            key = "lobby.navigator"
+            name = Component.text().color(NamedTextColor.AQUA).append(Component.text("Navigator")).build()
             lore = mutableListOf(
-                Component.text("§7Click to open the navigator")
+                Component.text().color(NamedTextColor.GRAY).append(Component.text("Click to open the navigator"))
+                    .build()
             )
         }
     },
@@ -29,7 +33,29 @@ object Navigator : UnassignedItemStack(material = { Material.COMPASS },
     }) {
     private val gui = BaseGui(GuiType.CHEST, 5) {
         {
-            title = Component.text("§aNavigator")
+            key = "lobby.navigator"
+            title = Component.text().color(NamedTextColor.AQUA).append(Component.text("Navigator")).build()
         }
+    }.apply {
+        setItem(5, UnassignedItemStack(material = { Material.NETHER_STAR },
+            languageProvider = {
+                {
+                    key = "lobby.navigator.spawn"
+                    name = Component.text().color(NamedTextColor.AQUA).append(Component.text("Spawn")).build()
+                    lore = mutableListOf(
+                        Component.text().color(NamedTextColor.GRAY).append(Component.text("Click to teleport"))
+                            .build()
+                    )
+                }
+            },
+            blockClick = { true },
+            blockInteract = { true },
+            blockDrop = { true },
+            clickHandler = { _, uuid ->
+                uuid.getPlayer()!!.teleport(Pos(0.0, 1.2, 0.0))
+                uuid.getPlayer()!!.closeInventory()
+                true
+            }
+        ))
     }
 }
