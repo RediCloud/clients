bc353a48eb23027560d87cbecc6ce82d8b825285
Suqatri <paul@suqatri.net>
2023-04-25 17:44:57 +0200

    fix: minestom starting

--- minestom-applications/default-application/src/main/kotlin/dev/redicloud/minestom/application/MinestomMain.kt
+++ minestom-applications/default-application/src/main/kotlin/dev/redicloud/minestom/application/MinestomMain.kt
@@ -7,6 +7,7 @@ import net.minestom.server.extras.velocity.VelocityProxy
 fun main() {
     server //to init the val
     if (System.getenv("VELOCITY_SECRET") != null) {
+        MinecraftServer.LOGGER.info("Using velocity proxy support!")
         VelocityProxy.enable(System.getenv("VELOCITY_SECRET"))
     } else {
         MinecraftServer.LOGGER.warn("Velocity secret not set, velocity support disabled!")
@@ -15,8 +16,11 @@ fun main() {
     if (System.getProperty("service.bind.host") == null || System.getProperty("service.bind.port") == null) {
         MinecraftServer.LOGGER.warn("Service bind host or port not set, using default values!")
     }
+    val address = System.getProperty("service.bind.host") ?: "127.0.0.1"
+    val port = (System.getProperty("service.bind.port") ?: "5000").toInt()
+    MinecraftServer.LOGGER.info("Starting on $address:$port")
     server.start(
-        System.getProperty("service.bind.host") ?: "127.0.0.1",
-        (System.getProperty("service.bind.port") ?: "5000").toInt()
+        address,
+        port
     )
 }
