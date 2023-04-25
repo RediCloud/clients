9e4a869b205d7feeda5f307c1234cc28cb8f0d85
Suqatri <paul@suqatri.net>
2023-04-25 17:45:20 +0200

    fix: minestom command

--- api/minestom/src/main/kotlin/dev/redicloud/clients/minestom/command/MinestomCommand.kt
+++ api/minestom/src/main/kotlin/dev/redicloud/clients/minestom/command/MinestomCommand.kt
@@ -6,7 +6,7 @@ import dev.redicloud.api.command.data.CommandData
 import net.minestom.server.entity.Player
 import net.minestom.server.command.builder.Command as MinestomCommand
 
-class MinestomCommand(
+open class MinestomCommand(
     override val commandName: String,
     override val commandAliases: Array<String>,
     override val commandDescription: String,
